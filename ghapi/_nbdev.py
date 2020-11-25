488c0090a1cb62de580a32e933cd54fec20f1769
Jeremy Howard <j@fast.ai>
2020-11-24 16:56:18 -0800

    refactor

--- ghapi/_nbdev.py
+++ ghapi/_nbdev.py
@@ -3,9 +3,11 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"GH_HOST": "00_core.ipynb",
+         "GhVerb": "00_core.ipynb",
          "GhApi": "00_core.ipynb",
          "GH_OPENAPI_URL": "90_build_lib.ipynb",
-         "build_funcs": "90_build_lib.ipynb"}
+         "build_funcs": "90_build_lib.ipynb",
+         "GhMeta": "90_build_lib.ipynb"}
 
 modules = ["core.py",
            "build_lib.py"]
