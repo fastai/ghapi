e4bf3a2e17ff443d884e65ea597bc08673260566
Jeremy Howard <j@fast.ai>
2020-12-01 15:46:43 -0800

    Update build-thankyou-pull_request.py

--- .github/scripts/build-thankyou-pull_request.py
+++ .github/scripts/build-thankyou-pull_request.py
@@ -1,2 +1,11 @@
 from fastcore.all import *
 from ghapi import *
+
+def reply_thanks():
+    payload = context_github.event
+    print(payload)
+    if payload.action != 'opened': return
+    api = GhApi(owner='fastai', repo='ghapi', token=github_token())
+    api.issues.create_comment(issue_number=payload.number, body='Thank you for your *valuable* contribution')
+
+reply_thanks()
