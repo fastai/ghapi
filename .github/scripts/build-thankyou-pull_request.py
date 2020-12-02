c7fb34b6b9b6b171f56a2d1a6392cf644af6077f
Jeremy Howard <j@fast.ai>
2020-12-02 09:46:23 -0800

    workflow dispatch

--- .github/scripts/build-thankyou-pull_request.py
+++ .github/scripts/build-thankyou-pull_request.py
@@ -3,8 +3,12 @@ from ghapi import *
 
 def reply_thanks():
     payload = context_github.event
-    if payload.action != 'opened': return
-    api = GhApi(owner='fastai', repo='ghapi', token=github_token())
-    api.issues.create_comment(issue_number=payload.number, body='Thank you for your *valuable* contribution')
+    if 'workflow' in payload: issue = 1
+    else:
+        if payload.action != 'opened': return
+        api = GhApi(owner='fastai', repo='ghapi', token=github_token())
+        issue = payload.number
+    api.issues.create_comment(issue_number=issue, body='Thank you for your *valuable* contribution')
 
 reply_thanks()
+
